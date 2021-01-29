140d1f92bc6c81fe50e90acffd13660dd5ca7103
yugank15 <yugankgupta15@gmail.com>
2021-01-29 15:37:55 +0530

    Changes in routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('/home', function () {
+Route::view('/aboutus', function () {
     return view('home');
 });
 
