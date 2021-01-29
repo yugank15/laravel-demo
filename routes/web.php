862adc9b31196235e05a5385f44a5b048856f607
yugank15 <yugankgupta15@gmail.com>
2021-01-29 15:20:09 +0530

    Change Route

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('Hello This is demo');
+Route::view('/home', function () {
+    return view('home');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
